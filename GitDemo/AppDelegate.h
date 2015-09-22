f811cd00d374972236d991a8a93376b3704b30ce
liulei <liulei@Potter.local>
2015-09-22 14:30:17 +0800

    Initial Commit

---
+++ GitDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitDemo
+//
+//  Created by liulei on 15/9/22.
+//  Copyright (c) 2015年 LL_. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitDemo
+//
+//  Created by liulei on 15/9/22.
+//  Copyright (c) 2015年 LL_. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
